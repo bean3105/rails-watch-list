9698ed828e89dedf5ecf3f6b2a38de3a8ee36811
Georgia Bean-Butler <georgia.bean@icloud.com>
2023-08-25 08:11:24 +0100

    watch list - rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # As a user, I can see all my movie lists
+  # As a user, I can create a movie list
+  # As a user, I can see the details of a movie list
+  # As a user, I can bookmark a movie inside a movie list
+  # As a user, I can destroy a bookmark
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
